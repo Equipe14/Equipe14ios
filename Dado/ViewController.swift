4ba45eff07c6f7b89628be219c646d9247d606a9
Equipe14 <equipe14,ioscapacitabr@gmail.com>
2025-05-22 20:49:38 -0300

    Initial Commit

---
+++ Dado/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dado
+//
+//  Created by iredefbmac_38 on 21/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
